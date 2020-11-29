3fc431d6b59e52bb2166d5e9077761f1fa65a699
Anmol Verma <anmol.verma4@gmail.com>
2020-11-29 12:37:29 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
 import logo from "./logo/lic-logo.png";
 import Gallery from "./Gallery";
 import ContactMe from "./ContactMe";
-import Avatar from "@material-ui/core/Avatar";
-import pic from "./logo/pic.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,11 +22,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function App() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
   return (
     <div>
@@ -44,24 +35,13 @@ export default function App() {
         }}
       >
         <img className={classes.logo} src={logo} alt="Logo" />
-        <ContactMe value={2} showHeader={true}></ContactMe>
+        <ContactMe showHeader={true}></ContactMe>
       </div>
 
       <div>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-          <Tab label="Home" />
-          <Tab label="About Me" />
-          <Tab label="Contact Me" />
-          <Tab label="Gallery" />
-        </Tabs>
-        <ContactMe value={value}></ContactMe>
-        <Gallery value={value}></Gallery>
+      <Gallery></Gallery>
+      <ContactMe></ContactMe>
+     
       </div>
     </div>
   );
--- src/ContactMe.js
+++ src/ContactMe.js
@@ -1,12 +1,8 @@
-import TabPanel from "./TabPanel";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import ReactWhatsapp from "react-whatsapp";
 import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import { green } from "@material-ui/core/colors";
 import EmailIcon from "@material-ui/icons/Email";
 import Linkify from "react-linkify";
 import PhoneNumber from "react-phone-number";
@@ -16,7 +12,6 @@ import pic from "./logo/pic.jpg";
 import { Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-
   cardHeader: {
     display: "flex",
     flexDirection: "column",
@@ -32,14 +27,20 @@ const useStyles = makeStyles((theme) => ({
     flex: 1,
     marginTop: 12,
   },
+  phone: {
+    color: green[500],
+  },
   bullet: {
     display: "inline-block",
     margin: "0 2px",
     transform: "scale(0.8)",
   },
   title: {
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: "bold",
+  },
+  email: {
+    fontSize: 14,
   },
   pos: {
     marginBottom: 12,
@@ -47,122 +48,185 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ContactMe(props) {
-  const { children, value, index,showHeader, ...other } = props;
+  const { showHeader } = props;
   const classes = useStyles();
 
   let content;
-  if(!showHeader){
-      content = <div>
-      <WhatsAppNumber phoneNumber={"+919815778354"} text={"Official Number"}></WhatsAppNumber>
-      <PhoneNumberCustom phoneNumber={"+919815778354"} ></PhoneNumberCustom>
-      <div className={classes.flexRow}>
-        <EmailIcon
-          color="primary"
-          fontSize="large"
-          element="label"
-          style={{ marginRight: 20 }}
-        ></EmailIcon>
-        <Linkify>jatinderkumargogna@gmail.com</Linkify>
+  if (showHeader) {
+    content = <Header></Header>;
+  } else {
+    content = (
+      <div>
+        <ContactInfo></ContactInfo>
       </div>
-      <WhatsAppNumber phoneNumber={"+918283848354"} text={"Alternate Number"}></WhatsAppNumber>
-      <PhoneNumberCustom phoneNumber={"+918283848354"} ></PhoneNumberCustom>
-      </div>
-
-  }else{
-    content = <div></div>
+    );
   }
 
   return (
-    <TabPanel value={value} index={2}>
+    <div style={{ marginRight: 10 }}>
       <Paper elevation={0}>
-      <div className={classes.cardHeader}>
-            <div className={classes.flexRow}>
-              <Avatar
-                alt="Jatinder Kumar Gogna"
-                src={pic}
-                className={classes.large}
-              />
-
-              <div className={classes.cardHeader} style={{ marginLeft: 20 }}>
-                <Typography
-                  className={classes.title}
-                  color="textPrimary"
-                  gutterBottom
-                >
-                  Jatinder Kumar Gogna
-                </Typography>
-                <Typography
-                  className={classes.title}
-                  color="textSecondary"
-                  gutterBottom
-                >
-                  Chief Life Insurance Advisor
-                </Typography>
-                <Typography
-                  className={classes.title}
-                  color="textSecondary"
-                  gutterBottom
-                >
-                  C.M. Club Member
-                </Typography>
-              </div>
-            </div>
-          
-            {content}
-           
-          </div>
+        <div className={classes.cardHeader}>{content}</div>
       </Paper>
-    </TabPanel>
+    </div>
   );
 }
 
-export function PhoneNumberCustom(props){
-    const { phoneNumber } = props;
-    const classes = useStyles();
-    return (<div className={classes.flexRow}>
-        <PhoneIcon
-          color="primary"
-          fontSize="large"
-          style={{ marginRight: 20 }}
-        ></PhoneIcon>
+export function PhoneNumberCustom(props) {
+  const { phoneNumber, text } = props;
+  const classes = useStyles();
+  return (
+    <div className={classes.flexRow}>
+      <PhoneIcon fontSize="large" style={{ marginRight: 20 }}></PhoneIcon>
+      <div className={classes.cardHeader}>
+        <Typography className={classes.title} color="textPrimary" gutterBottom>
+          {text}
+        </Typography>
         <PhoneNumber
           number={phoneNumber}
           isLinked={true}
           element="label"
         ></PhoneNumber>
-      </div>)
+      </div>
+    </div>
+  );
 }
 
+export function WhatsAppNumber(props) {
+  const { phoneNumber, text } = props;
+  const classes = useStyles();
 
-export function WhatsAppNumber(props){
-    const { phoneNumber,text } = props;
-    const classes = useStyles();
-
-    return (<ReactWhatsapp
-        number = {phoneNumber}
-        message="Hi! I want to connect with you."
-        element="div"
-      >
-        <div className={classes.flexRow}>
-          <WhatsAppIcon
-            color="primary"
-            fontSize="large"
-            style={{ marginRight: 20 }}
-          ></WhatsAppIcon>{" "}
-          <div className={classes.cardHeader}>
-            <Typography
-              className={classes.title}
-              color="textPrimary"
-              gutterBottom
-            >
-              {text}
-            </Typography>
-            <PhoneNumber
-              number={phoneNumber}
-              isLinked={false}
-              element="label"
-            ></PhoneNumber>
-          </div>
+  return (
+    <ReactWhatsapp
+      number={phoneNumber}
+      message="Hi! I want to connect with you."
+      element="div"
+    >
+      <div className={classes.flexRow}>
+        <WhatsAppIcon
+          className={classes.phone}
+          fontSize="large"
+          style={{ marginRight: 10 }}
+        ></WhatsAppIcon>{" "}
+        <div className={classes.cardHeader}>
+          <Typography
+            className={classes.title}
+            color="textPrimary"
+            gutterBottom
+          >
+            {text}
+          </Typography>
+          <PhoneNumber
+            number={phoneNumber}
+            isLinked={false}
+            element="label"
+          ></PhoneNumber>
         </div>
-      </ReactWhatsapp>)
+      </div>
+    </ReactWhatsapp>
+  );
+}
+
+export function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.flexRow}>
+      <Avatar alt="Jatinder Kumar Gogna" src={pic} className={classes.large} />
+
+      <div className={classes.cardHeader} style={{ marginLeft: 5 }}>
+        <Typography className={classes.title} color="textPrimary" gutterBottom>
+          Jatinder Kumar Gogna
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Chief Life Insurance Advisor
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          C.M. Club Member
+        </Typography>
+      </div>
+    </div>
+  );
+}
+
+export function ContactInfo() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <WhatsAppNumber
+          phoneNumber={"+919815778354"}
+          text={"WhatsApp"}
+        ></WhatsAppNumber>
+        <PhoneNumberCustom
+          phoneNumber={"+919815778354"}
+          text={"Call"}
+        ></PhoneNumberCustom>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <WhatsAppNumber
+          phoneNumber={"+918283848354"}
+          text={"WhatsApp"}
+        ></WhatsAppNumber>
+        <PhoneNumberCustom
+          phoneNumber={"+918283848354"}
+          text={"Call"}
+        ></PhoneNumberCustom>
+      </div>
+      <EmailView></EmailView>
+      <div style={{marginBottom:50}}></div>
+    </div>
+  );
+}
+
+export function EmailView() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.flexRow}>
+      <EmailIcon
+        fontSize="large"
+        element="label"
+        style={{ marginRight: 20 }}
+      ></EmailIcon>
+      <div className={classes.cardHeader}>
+        <Typography className={classes.title} color="textPrimary" gutterBottom>
+          Email Me
+        </Typography>
+        <Linkify>
+          <Typography className={classes.email}>
+            jatinderkumargogna@gmail.com
+          </Typography>
+        </Linkify>
+      </div>
+    </div>
+  );
 }
